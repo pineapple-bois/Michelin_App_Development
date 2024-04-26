e0a8244d59b554080daf76873a86f18c1e597c1d
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-04-26 23:02:58 +0100

    Fundamental restructure

--- layouts/layout_main.py
+++ layouts/layout_main.py
@@ -2,75 +2,99 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
-def get_main_layout(unique_regions, star_descriptions):
+def michelin_star():
+    return html.Img(src="https://upload.wikimedia.org/wikipedia/commons/a/ad/MichelinStar.svg",
+                    className='michelin-star',
+                    style={'width': '20px', 'vertical-align': 'middle'})
+
+
+def bib_gourmand():
+    return html.Img(src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Michelin_Bib_Gourmand.png",
+                    className='bib-image',
+                    style={'width': '20px', 'vertical-align': 'middle'})
+
+
+def get_main_layout(unique_regions):
     # Title Section with Container Style
     title_section = html.Div(children=[
-        html.H1("Michelin Guide to France 2024", className='title-section'),
-        html.P([
-            "Explore the finest culinary destinations in France, as reviewed by Michelin.",
-            html.Br(),  # Add a line break
-            "France is divided administratively into regions and departments.",
-            html.Br(),  # Another line break for spacing if needed
-            html.Br(),  # Add as many <br> as needed for desired spacing
-            "Select a region to see the Michelin-starred restaurants by department."  # Instructional sentence
-        ], className='title-description')],
-        className='container-style'
-    )
-
-    # Dropdown Section with Container Style
-    dropdown_section = html.Div([
-        dbc.Row([
-            dbc.Col(dcc.Dropdown(
-                id='region-dropdown',
-                options=[{'label': region, 'value': region} for region in unique_regions],
-                value=unique_regions[0],  # Default value
-                className='dropdown-style',
-                clearable=False
-            )),
-            dbc.Col(dcc.Dropdown(
-                id='department-dropdown',
-                className='dropdown-style'
-            ))
-        ])
+        html.H1(["Michelin Guide to France. ", html.Span("2024", className='year-text')], className='title-section')
     ], className='container-style')
 
-    # Map Display Section with Container Style
-    map_display_section = html.Div([
+    ratings_layout = html.Div([
+        dbc.Row([
+            # First Column
+            dbc.Col([
+                html.P([michelin_star(), michelin_star(), michelin_star(),],className='star-description-title'),
+                html.P('Exceptional cuisine', className='star-description-title'),
+                html.P('Worth a special journey', className='star-description-text'),
+                html.Br(),
+                html.P(michelin_star(), className='star-description-title'),
+                html.P('High-quality cooking', className='star-description-title'),
+                html.P('Worth a stop', className='star-description-text'),
+            ], width=6),
+            # Second Column
+            dbc.Col([
+                html.P([michelin_star(), michelin_star()], className='star-description-title'),
+                html.P('Excellent cooking', className='star-description-title'),
+                html.P('Worth a detour', className='star-description-text'),
+                html.Br(),
+                html.P([bib_gourmand()], className='star-description-title'),
+                html.P('Bib Gourmand', className='star-description-title'),
+                html.P('Exceptionally good food at moderate prices', className='star-description-text'),
+            ], width=6)
+        ], className='ratings-row')
+    ], className='ratings-container')
+
+    # New Container Section with Map and Star Ratings Side by Side
+    map_and_star_section = html.Div([
         dbc.Row([
             dbc.Col(dcc.Graph(
                 id='map-display',
                 responsive=True,
                 className='map-display',
-                config={
-                    'displayModeBar': True,
-                    'scrollZoom': True,
-                    'modeBarButtonsToRemove': ['pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d',
-                                               'resetScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian',
-                                               'toggleSpikelines', 'toImage'],
-                    'modeBarButtonsToAdd': ['zoom2d', 'resetScale2d']
-                }
-            ))
-        ])
-    ], className='container-style')
-
-    # Star Descriptions Section with Container Style
-    star_descriptions_section = html.Div([
-        dbc.Row([
+                # ... [graph config]
+            ), width=8),  # Set width as needed
             dbc.Col([
-                html.Div([html.Div([
-                    html.Img(src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Michelin_Bib_Gourmand.png", className='star-image'),
-                    html.Span(star_descriptions[key], className='star-description-text')
-                ], className='star-description-item') if key == 0.5 else
-                html.H6(star_descriptions[key], className='star-description')
-                ]) for key in star_descriptions
-            ])
+                # Container for Site Description
+                html.Div([
+                    html.H5("Explore the finest culinary destinations in France, as reviewed by Michelin.",
+                            className='site-description')
+                ], className='description-container'),
+
+                # Container for Instructions
+                html.Div([
+                    html.P("France is divided administratively into regions and departments. "
+                           "Select a region to see the Michelin-starred restaurants by department.",
+                           className='instructions')
+                ], className='instructions-container'),
+
+                # Container for Region Dropdown
+                html.Div([
+                    html.H6("Select a Region", className='regions-title'),
+                    dcc.Dropdown(
+                        id='region-dropdown',
+                        options=[{'label': region, 'value': region} for region in unique_regions],
+                        value=unique_regions[0],  # Default value
+                        className='dropdown-style',
+                        clearable=False
+                    )
+                ], className='dropdown-container'),
+
+                # Container for Department Dropdown
+                html.Div([
+                    html.H6("Select a Department", className='departments-title'),
+                    dcc.Dropdown(
+                        id='department-dropdown',
+                        className='dropdown-style'
+                    )
+                ], className='dropdown-container'),
+                ratings_layout
+            ], className='sidebar-container', width=4)
         ])
     ], className='container-style')
 
-    # Combine all sections into the main layout with Container Style
+    # Combine all sections into the main layout
     return html.Div([
         title_section,
-        dropdown_section,
-        map_display_section,
-        star_descriptions_section])
-
+        map_and_star_section  # Replace map_display_section with map_and_star_section
+    ])
