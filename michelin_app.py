323d35740fef4efd64b13fe70bab4b8915e231ca
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-04-26 19:27:06 +0100

    Amended update_map callback

--- michelin_app.py
+++ michelin_app.py
@@ -6,11 +6,11 @@ import os
 import json
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc, html
+from dash import dcc, html, callback_context
 from dash.dependencies import Input, Output
 from flask import Flask, redirect, request
 from layouts.layout_main import get_main_layout
-from appFunctions import plot_interactive_department
+from appFunctions import plot_regional_outlines, plot_interactive_department
 
 
 # # FOR LOCAL DEVELOPMENT ONLY - RISK MAN-IN-MIDDLE ATTACKS
@@ -24,15 +24,29 @@ url = ("https://raw.githubusercontent.com/pineapple-bois/Michelin_Rated_Restaura
 all_france = pd.read_csv(url)
 
 # Load GeoJSON departmental data
-geojson_url = ("https://raw.githubusercontent.com/pineapple-bois/Michelin_Rated_Restaurants/"
-               "main/Years/2024/data/France/geodata/department_restaurants.geojson")
-geo_df = gpd.read_file(geojson_url)
+deptjson_url = ("https://raw.githubusercontent.com/pineapple-bois/Michelin_Rated_Restaurants/"
+                "main/Years/2024/data/France/geodata/department_restaurants.geojson")
+geo_df = gpd.read_file(deptjson_url)
+
+# Load GeoJSON regional data
+regionjson_url = ("https://raw.githubusercontent.com/pineapple-bois/Michelin_Rated_Restaurants/"
+                  "main/Years/2024/data/France/geodata/region_restaurants.geojson")
+region_df = gpd.read_file(regionjson_url)
+
 
 # Get unique department numbers with restaurants
 departments_with_restaurants = all_france['department_num'].unique()
-
 # Filter geo_df
 geo_df = geo_df[geo_df['code'].isin(departments_with_restaurants)]
+
+
+# Use geo_df to get unique regions and departments for the initial dropdowns
+unique_regions = geo_df['region'].unique()
+initial_departments = geo_df[geo_df['region'] == unique_regions[0]][['department', 'code']].drop_duplicates().to_dict('records')
+initial_options = [{'label': f"{dept['department']} ({dept['code']})", 'value': dept['department']} for dept in initial_departments]
+dept_to_code = geo_df.drop_duplicates(subset='department').set_index('department')['code'].to_dict()
+region_to_name = {region: region for region in geo_df['region'].unique()}
+
 
 star_descriptions = {
     3: "⭐⭐⭐ - Exceptional cuisine, worth a special journey",
@@ -40,12 +54,6 @@ star_descriptions = {
     1: "⭐ - High-quality cooking, worth a stop",
     0.5: "- Bib Gourmand - Exceptionally good food at moderate prices"
 }
-
-# Use geo_df to get unique regions and departments for the initial dropdowns
-unique_regions = geo_df['region'].unique()
-initial_departments = geo_df[geo_df['region'] == unique_regions[0]][['department', 'code']].drop_duplicates().to_dict('records')
-initial_options = [{'label': f"{dept['department']} ({dept['code']})", 'value': dept['department']} for dept in initial_departments]
-dept_to_code = geo_df.drop_duplicates(subset='department').set_index('department')['code'].to_dict()
 
 
 # Initialize the Dash app
@@ -84,35 +92,41 @@ def update_department_dropdown(selected_region):
 
 @app.callback(
     Output('map-display', 'figure'),
-    [Input('department-dropdown', 'value')]
+    [Input('department-dropdown', 'value'),
+     Input('region-dropdown', 'value')]
 )
-def update_map(selected_department):
+def update_map(selected_department, selected_region):
     # Show all stars by default
     selected_stars = [0.5, 1, 2, 3]
-    if selected_department is None:
-        # Create an empty figure with map centered around France
-        fig = go.Figure(go.Scattermapbox())
 
-        fig.update_layout(
-            plot_bgcolor='black',
-            paper_bgcolor='black',
-            title="Michelin Guide to France 2024",
+    # Determine which input triggered the callback
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if selected_department:
+        # If a department is selected, show specific department information
+        department_code = dept_to_code[selected_department]
+        return plot_interactive_department(all_france, geo_df, department_code, selected_stars)
+    elif selected_region or trigger_id == 'region-dropdown':
+        # If a region is selected (and it's the trigger), show regional outlines
+        region_name = region_to_name[selected_region]
+        return plot_regional_outlines(region_df, region_name)
+    else:
+        # No specific region or department selected, or department cleared
+        # Create an empty figure with map centered around France
+        return go.Figure(go.Scattermapbox()).update_layout(
             font=dict(
                 family="Courier New, monospace",
                 size=18,
                 color="white"
             ),
-            width=1000,
-            height=800,
+            width=800,
+            height=600,
             mapbox_style="carto-positron",
-            mapbox_zoom=5,
             mapbox_center_lat=46.603354,  # Approximate latitude for France center
-            mapbox_center_lon=1.888334  # Approximate longitude for France center
+            mapbox_center_lon=1.888334,  # Approximate longitude for France center
+            margin={"r": 20, "t": 20, "l": 20, "b": 20},  # Remove margins
         )
-        return fig
-    department_code = dept_to_code[selected_department]
-    fig = plot_interactive_department(all_france, geo_df, department_code, selected_stars)
-    return fig
 
 
 # For local development, debug=True
