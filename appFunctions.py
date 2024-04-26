45a8ad3a741664370f98f5cd999b7ac318e154ca
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-04-26 19:02:45 +0100

    Added a region plotting function

--- appFunctions.py
+++ appFunctions.py
@@ -3,6 +3,49 @@ import geopandas as gpd
 import plotly.graph_objects as go
 
 
+def plot_regional_outlines(region_df, region):
+    fig = go.Figure(go.Scattermapbox())  # Initialize empty figure with mapbox
+
+    # Filter the GeoDataFrame for the selected region
+    filtered_region = region_df[region_df['region'] == region]
+
+    # Loop through the filtered GeoDataFrame
+    for _, row in filtered_region.iterrows():
+        geometry = row['geometry']
+        if geometry.geom_type == 'Polygon':
+            x, y = geometry.exterior.xy
+            fig.add_trace(go.Scattermapbox(
+                lat=list(y),
+                lon=list(x),
+                mode='lines',
+                line=dict(width=1, color='black'),
+                hoverinfo='none',
+                showlegend=False
+            ))
+        elif geometry.geom_type == 'MultiPolygon':
+            for poly in geometry.geoms:
+                x, y = poly.exterior.xy
+                fig.add_trace(go.Scattermapbox(
+                    lat=list(y),
+                    lon=list(x),
+                    mode='lines',
+                    line=dict(width=1, color='black'),
+                    hoverinfo='none',
+                    showlegend=False
+                ))
+
+    # Update map layout settings
+    fig.update_layout(
+        mapbox_style="carto-positron",
+        mapbox_zoom=5,  # Zoom level to show all of France
+        mapbox_center_lat=46.603354,  # Approximate latitude for France center
+        mapbox_center_lon=1.888334,  # Approximate longitude for France center
+        margin={"r":20,"t":20,"l":20,"b":20},  # Remove margins
+        showlegend=False
+    )
+    return fig
+
+
 def plot_interactive_department(data_df, geo_df, department_code, selected_stars):
     # Initialize a blank figure
     fig = go.Figure()
@@ -49,40 +92,33 @@ def plot_interactive_department(data_df, geo_df, department_code, selected_stars
         axis=1
     )
 
-    # Overlay restaurant points
+    # Overlay restaurant points without adding them to the legend
     for star, color in color_map.items():
         subset = dept_data[dept_data['stars'] == star]
-
-        # Adjust hover text for Bib Gourmand
-        if star == 0.5:
-            label_name = 'Bib Gourmand'
-        else:
-            label_name = f"{'⭐' * int(star)}"
-
-        fig.add_trace(go.Scattermapbox(lat=subset['latitude'],
-                                       lon=subset['longitude'],
-                                       mode='markers',
-                                       marker=go.scattermapbox.Marker(size=10, color=color),
-                                       text=subset['hover_text'],
-                                       hovertemplate='%{text}<br>Coordinates: (%{lat}, %{lon})',
-                                       name=label_name))
+        fig.add_trace(go.Scattermapbox(
+            lat=subset['latitude'],
+            lon=subset['longitude'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(size=10, color=color),
+            text=subset['hover_text'],
+            hovertemplate='%{text}<extra></extra>',  # This hides the trace name from the hover label
+            showlegend=False  # This ensures that the trace isn't added to the legend
+        ))
 
     # Adjusting layouts
     fig.update_layout(
-        plot_bgcolor='black',
-        paper_bgcolor='black',
-        title="Michelin Guide to France 2024",
         font=dict(
             family="Courier New, monospace",
             size=18,
             color="white"
         ),
-        width=1000,
-        height=800,
+        width=800,
+        height=600,
         mapbox_style="carto-positron",
         mapbox_zoom=8,
         mapbox_center_lat=dept_data['latitude'].mean(),
-        mapbox_center_lon=dept_data['longitude'].mean()
+        mapbox_center_lon=dept_data['longitude'].mean(),
+        margin={"r": 20, "t": 20, "l": 20, "b": 20},  # Remove margins
     )
 
     return fig
