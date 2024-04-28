5eef6540e9184d11cb7a52ff305a51a520e9d7d5
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-04-28 16:54:48 +0100

    Updated restaurant details html

--- appFunctions.py
+++ appFunctions.py
@@ -2,7 +2,6 @@ import pandas as pd
 import geopandas as gpd
 import plotly.graph_objects as go
 from dash import html, dcc
-from layouts.layout_main import michelin_star, bib_gourmand
 
 
 def plot_regional_outlines(region_df, region):
@@ -48,8 +47,20 @@ def plot_regional_outlines(region_df, region):
     return fig
 
 
+def michelin_star(count):
+    # Returns a list of image components for each star
+    return [html.Img(src="https://upload.wikimedia.org/wikipedia/commons/a/ad/MichelinStar.svg",
+                     className='michelin-star',
+                     style={'width': '20px', 'vertical-align': 'middle', 'margin-right': '3px'}) for _ in range(int(count))]
+
+
+def bib_gourmand():
+    return html.Img(src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Michelin_Bib_Gourmand.png",
+                    className='bib-image',
+                    style={'width': '20px', 'vertical-align': 'middle'})
+
+
 def get_restaurant_details(row):
-    # Extract information from the DataFrame row
     name = row['name']
     stars = row['stars']
     cuisine = row['cuisine']
@@ -58,14 +69,37 @@ def get_restaurant_details(row):
     location = row['location']
     website_url = row['url']
 
-    # Create HTML content to display this information
+    # Determine if it's a Bib Gourmand or how many Michelin stars
+    if stars == 0.5:
+        star_component = bib_gourmand()
+    else:
+        star_component = michelin_star(stars)
+
+    # Create HTML content to display this information, organized in divs
     details_layout = html.Div([
-        html.H3(f"{'★' * int(stars)} {name}", className='restaurant-name'),
-        html.P(f"Cuisine: {cuisine}", className='restaurant-cuisine'),
-        html.P(f"Price Range: {price}", className='restaurant-price'),
-        html.P(address, className='restaurant-address'),
-        html.P(location, className='restaurant-location'),
-        html.A("Visit Website", href=website_url, target='_blank', className='restaurant-website')
+        html.Div([
+            html.Div([
+                html.Span(name, className='restaurant-name'),
+                html.Span(star_component, className='restaurant-stars'),  # Use Span for in-line display
+            ], className='details-header'),
+            html.Div([
+                html.Span(f"{cuisine}", className='restaurant-cuisine')
+            ], className='details-cuisine'),
+            html.Div([
+                html.Span(f"{price}", className='restaurant-price')
+            ], className='details-price'),
+        ], className='restaurant-info'),
+        html.Div([
+            html.Div([
+                html.Span(f"{address}", className='restaurant-address')
+            ], className='details-address'),
+            html.Div([
+                html.Span(f"{location}", className='restaurant-location')
+            ], className='details-location'),
+        ], className='address-info'),
+        html.Div([
+            html.A("Visit Website", href=website_url, target='_blank', className='restaurant-website', style={'display': 'block', 'marginTop': '10px'})
+        ], className='details-website')
     ], className='restaurant-details')
 
     return details_layout
@@ -171,107 +205,3 @@ def plot_interactive_department(data_df, geo_df, department_code, selected_stars
     )
 
     return fig
-
-
-# def plot_interactive_department(data_df, geo_df, department_code, selected_stars):
-#     # Initialize a blank figure
-#     fig = go.Figure()
-#     fig.update_layout(autosize=True)
-#
-#     # Get the specific geometry
-#     specific_geometry = geo_df[geo_df['code'] == str(department_code)]['geometry'].iloc[0]
-#
-#     # Plot the geometry's boundaries
-#     if specific_geometry.geom_type == 'Polygon':
-#         x, y = specific_geometry.exterior.xy
-#         fig.add_trace(go.Scattermapbox(
-#             lat=list(y),
-#             lon=list(x),
-#             mode='lines',
-#             line=dict(width=0.5, color='black'),  # Making line thicker and black for visibility
-#             hoverinfo='none',
-#             showlegend=False  # Hide from legend
-#         ))
-#     elif specific_geometry.geom_type == 'MultiPolygon':
-#         for polygon in specific_geometry.geoms:
-#             if polygon.geom_type == 'Polygon':  # Ensure we're dealing with a Polygon
-#                 x, y = polygon.exterior.xy
-#                 fig.add_trace(go.Scattermapbox(
-#                     lat=list(y),
-#                     lon=list(x),
-#                     mode='lines',
-#                     line=dict(width=0.5, color='black'),
-#                     hoverinfo='none',
-#                     showlegend=False
-#                 ))
-#
-#     # Tooltip section
-#     color_map = {
-#         0.5: "#640A64",
-#         1: "#FFB84D",
-#         2: "#FE6F64",
-#         3: "#C2282D"  # (r, g, b, opacity)
-#     }
-#
-#     text_color_map = {
-#         0.5: "#FFB84D",
-#         1: "#640A64",
-#         2: "#640A64",
-#         3: "#FFB84D"
-#     }
-#
-#     dept_data = data_df[(data_df['department_num'] == str(department_code)) & (data_df['stars'].isin(selected_stars))].copy()
-#     dept_data['color'] = dept_data['stars'].map(color_map)
-#
-#     # Modify the hover text function
-#     dept_data['hover_text'] = dept_data.apply(
-#         lambda row: f"<span style=\"font-family: 'Libre Franklin', sans-serif; font-size: 13px; color: {text_color_map[row['stars']]};\">"
-#                     f"<span style='font-size: 16px;'>{'🍽️' if row['stars'] == 0.5 else '★' * int(row['stars'])} {row['name']}</span><br>"
-#                     f"<span style='font-size: 13px;'>{row['cuisine']}</span><br>"
-#                     f"<span style='font-size: 13px;'>{row['price']}</span><br>"
-#                     f"<br>"
-#                     f"{row['address']}, {row['location']}<br>"
-#                     f"<br>"
-#                     f"<a href='{row['url']}' target='_blank' class='link-highlight' style='font-family: Libre Franklin, sans-serif; font-size: 15px; color: {text_color_map[row['stars']]}'>Visit website</a>",
-#         axis=1
-#     )
-#
-#     # Overlay restaurant points without adding them to the legend
-#     for star, color in color_map.items():
-#         subset = dept_data[dept_data['stars'] == star]
-#
-#         # Adjust hover text for Bib Gourmand
-#         if star == 0.5:
-#             label_name = '🍽️'
-#         else:
-#             label_name = f"{'★' * int(star)}"
-#
-#         fig.add_trace(go.Scattermapbox(
-#             lat=subset['latitude'],
-#             lon=subset['longitude'],
-#             mode='markers',
-#             marker=go.scattermapbox.Marker(size=11, color=color),
-#             text=subset['hover_text'],
-#             hovertemplate='%{text}',
-#             name=label_name,
-#             showlegend=False  # This ensures that the trace isn't added to the legend
-#         ))
-#
-#     # Adjusting layouts
-#     fig.update_layout(
-#         font=dict(
-#             family="Courier New, monospace",
-#             size=18,
-#             color="white"
-#         ),
-#         width=800,
-#         height=600,
-#         hoverdistance=40,
-#         mapbox_style="carto-positron",
-#         mapbox_zoom=8,
-#         mapbox_center_lat=dept_data['latitude'].mean(),
-#         mapbox_center_lon=dept_data['longitude'].mean(),
-#         margin={"r": 0, "t": 0, "l": 0, "b": 0},  # Remove margins
-#     )
-#
-#     return fig
